43598c1f576e9b366e757fd9082efeb0954b54f7
Loubnaelghazi <114692135+Loubnaelghazi@users.noreply.github.com>
2023-05-09 16:10:57 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-/* au  lieur kul file nderu feh import dkul hja . rdi n7tuhum kmlin hna urnb9aw n3utu rer 3la had index  */
+import React from "react";
+// hdi mb9atch druri n7thuha LI LFU9
+import ReactDom from "react-dom/client";
 
-export {default as  Header} from './Header'
-export { default as Container} from "./Container";
-export { default as Footer} from "./Footer";
-export {default as Hero} from "./Hero";
-export{default as MostPopular} from "./MostPopular";
-export { default as Library } from "./Library";
-export {default as PrimaryButton} from "../components/Buttons/Buttons";
-export{SecondaryButton as SecondaryButton} from "../components/Buttons/Buttons";
+import First from "./First";
+const root = ReactDom.createRoot(document.getElementById("root"));
+root.render(
+
+<First />
+
+
+
+);
